ad3b3aace7a11df891b83a238ce3054e34f850c2
feruz <fmiboy@gmail.com>
2025-09-22 12:42:05 +0300

    show auth method/icon

--- lib/features/user/presentation/multi_account/widgets/multi_account_dialog_item.dart
+++ lib/features/user/presentation/multi_account/widgets/multi_account_dialog_item.dart
@@ -4,6 +4,7 @@ import 'package:waves/core/common/extensions/ui.dart';
 import 'package:waves/core/common/widgets/custom_list_tile.dart';
 import 'package:waves/core/common/widgets/images/user_profile_image.dart';
 import 'package:waves/core/locales/locale_text.dart';
+import 'package:waves/core/utilities/enum.dart';
 import 'package:waves/features/auth/models/user_auth_model.dart';
 import 'package:waves/features/user/presentation/multi_account/controller/multi_account_switch_controller.dart';
 
@@ -21,20 +22,15 @@ class MultiAccountDialogItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = context.read<MultiAccountController>();
     final userName = item.accountName;
+    final showRemoveButton = currentUserName != item.accountName;
     return CustomListTile(
       onTap: () => _onTap(controller, context),
       leading: UserProfileImage(radius: 17, url: userName),
       titleText: userName,
-      trailing: Visibility(
-        visible: currentUserName != item.accountName,
-        child: IconButton(
-            onPressed: () {
-              controller.removeAccount(userName);
-            },
-            icon: const Icon(
-              Icons.close,
-              size: 20,
-            )),
+      trailing: _TrailingIcons(
+        authType: item.authType,
+        showRemoveButton: showRemoveButton,
+        onRemove: () => controller.removeAccount(userName),
       ),
     );
   }
@@ -46,5 +42,79 @@ class MultiAccountDialogItem extends StatelessWidget {
     }
 
     Navigator.pop(context);
+  }
+}
+
+class _TrailingIcons extends StatelessWidget {
+  const _TrailingIcons({
+    required this.authType,
+    required this.showRemoveButton,
+    required this.onRemove,
+  });
+
+  final AuthType authType;
+  final bool showRemoveButton;
+  final VoidCallback onRemove;
+
+  @override
+  Widget build(BuildContext context) {
+    final children = <Widget>[];
+    final loginIcon = _buildLoginIcon();
+
+    if (loginIcon != null) {
+      children.add(loginIcon);
+    }
+
+    if (showRemoveButton) {
+      if (children.isNotEmpty) {
+        children.add(const SizedBox(width: 8));
+      }
+      children.add(IconButton(
+        onPressed: onRemove,
+        icon: const Icon(
+          Icons.close,
+          size: 20,
+        ),
+      ));
+    }
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: children,
+    );
+  }
+
+  Widget? _buildLoginIcon() {
+    final asset = _authAssetForType(authType);
+    if (asset != null) {
+      return SizedBox(
+        height: 24,
+        width: 24,
+        child: Image.asset(
+          asset,
+          fit: BoxFit.contain,
+        ),
+      );
+    }
+
+    return const Icon(
+      Icons.key,
+      size: 20,
+    );
+  }
+
+  String? _authAssetForType(AuthType type) {
+    switch (type) {
+      case AuthType.postingKey:
+        return 'assets/images/auth/hive-signer-logo.png';
+      case AuthType.ecency:
+        return 'assets/images/auth/ecency-logo.png';
+      case AuthType.hiveKeyChain:
+        return 'assets/images/auth/hive-keychain-logo.png';
+      case AuthType.hiveAuth:
+        return 'assets/images/auth/hiveauth_icon.png';
+      case AuthType.hiveSign:
+        return 'assets/images/auth/hive-signer-logo.png';
+    }
   }
 }
